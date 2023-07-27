b39adcc89fd99413861f3ec9ede32d57ded27521
wj.jiang <1049895015@qq.com>
2023-07-27 13:59:10 +0800

    feat: 优化npm源

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,17 @@
+/*
+ * @Author: wj.jiang
+ * @Date: 2023-05-29 14:50:20
+ * @LastEditors: wj.jiang
+ * @LastEditTime: 2023-07-27 13:38:47
+ */
 import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
+  esm: {
+    output: 'dist',
+    ignores: [
+      'src/**/demo/**', // 避免打包demo文件到npm包里面
+    ],
+  },
 });
